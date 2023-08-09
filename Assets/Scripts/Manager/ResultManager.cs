3d42218f0af19cf54c87d3bd152c71e974f18626
Pota <haechan9502@gmail.com>
2023-08-10 05:47:33 +0900

    ResultScene finished

--- Assets/Scripts/Manager/ResultManager.cs
+++ Assets/Scripts/Manager/ResultManager.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Playables;
 using TMPro;
 
@@ -11,21 +13,45 @@ public class ResultManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI cardPlayedText;
     [SerializeField] TextMeshProUGUI score;
 
+    [SerializeField] Sprite[] rtanSprites;
+    [SerializeField] Image rtanImage;
+    [SerializeField] TextMeshProUGUI rtanText;
+
+    float playedTime = GameManager.I.time;
+    int   cardPlayed = GameManager.I.cardPlayed;
+    int   calculatedScore = 10000 -
+                          (int)
+                          (Mathf.Clamp((GameManager.I.time * 100) - 3200, 0, 5000) +
+                           Mathf.Clamp((GameManager.I.cardPlayed * 100) - 2800, 0, 5000));
+
     Queue<GameObject> objQueue = new Queue<GameObject>();
 
     private void Start()
     {
         Core.I.stage[Core.I.stageIndex] = true;
 
-        playedTimeText.text = GameManager.I.time.ToString("N2") + "초";
-        cardPlayedText.text = GameManager.I.cardPlayed + "회";
-
-        int calculatedScore = 10000 - 
-            (int)
-            (Mathf.Clamp((GameManager.I.time * 100) - 3200, 0, 5000) + 
-            Mathf.Clamp((GameManager.I.cardPlayed * 100) - 2800, 0, 5000));
-
         score.text          = "<color=yellow>SCORE</color>\n" + calculatedScore;
+
+        if(calculatedScore == 10000)
+        {
+            rtanImage.sprite = rtanSprites[3];
+            rtanText.text = "어떻게 이런 점수가?";
+        }
+        else if (calculatedScore >= 7500)
+        {
+            rtanImage.sprite = rtanSprites[2];
+            rtanText.text = "꽤 하는구만";
+        }
+        else if (calculatedScore >= 5000)
+        {
+            rtanImage.sprite = rtanSprites[1];
+            rtanText.text = "나쁘지 않군";
+        }
+        else
+        {
+            rtanImage.sprite = rtanSprites[0];
+            rtanText.text = "좀 더\n노력해라";
+        }
 
         fadeInOut.SetTrigger("FadeOut");
 
@@ -35,6 +61,16 @@ public class ResultManager : MonoBehaviour
         }
 
         GetComponent<PlayableDirector>().Play();
+    }
+
+    public void PlayPlayedTimeText()
+    {
+        StartCoroutine(NumberAnimation(playedTimeText, playedTime, "초", true));
+    }
+
+    public void PlayCardPlayedText()
+    {
+        StartCoroutine(NumberAnimation(cardPlayedText, cardPlayed, "회", false));
     }
 
     public void OnClickRetry()
@@ -97,6 +133,8 @@ public class ResultManager : MonoBehaviour
         }
 
         GameManager.I.StageInit(Core.I.stageIndex);
+        GameUIManager.I.ToggleGameOverUI();
+        GameUIManager.I.FadeOut();
         Core.I.UnloadLastAdditiveScene();
     }
 
@@ -117,5 +155,39 @@ public class ResultManager : MonoBehaviour
         {
             objQueue.Dequeue().SetActive(true);
         }
+    }
+
+    IEnumerator NumberAnimation(TextMeshProUGUI _text, float _value, string _lastText, bool _isFloat)
+    {
+        float timePassed = 0.0f;
+        float duration = 1.5f;
+
+        float startValue = 0.0f;
+
+        while(timePassed <= duration)
+        {
+            float t = timePassed / duration;
+
+            float calculatedValue = Mathf.Lerp(startValue, _value,t);
+            if(_isFloat == true)
+            {
+                _text.text = calculatedValue.ToString("N2") + _lastText;
+            }
+            else
+            {
+                _text.text = (int)calculatedValue + _lastText;
+            }
+
+            timePassed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        if(_isFloat == false)
+        {
+            _text.text = cardPlayed + _lastText;
+        }
+
+        yield break;
     }
 }
