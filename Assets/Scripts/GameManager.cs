53e3116e5e3ce2a7f42b644d918aa47c131e95ec
koju2001 <koju200555@gmail.com>
2023-08-09 09:31:11 +0900

    Testlevel.I.index = 값에따라 스테이지 결정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,8 +15,6 @@ public class GameManager : MonoBehaviour
     public static GameManager I;
     public GameObject firstCard;
     public GameObject secondCard;
-
-   
     void Awake()
     {
         I = this;
@@ -22,25 +22,65 @@ public class GameManager : MonoBehaviour
 
     // Start is called before the first frame update
 
-
     void Start()
     {
+        int Stageindex;
+        Stageindex = Testlevel.I.index;
+        Debug.Log(Stageindex);
         int[] ks = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
-
         ks = ks.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
-
-        for (int i = 0; i < 16; i++)
+        if (Stageindex == 0)
         {
-            GameObject newCard = Instantiate(card);
-            newCard.transform.parent = GameObject.Find("Cards").transform;
+            for (int i = 0; i < 16; i++)
+            {
+                GameObject newCard = Instantiate(card);
+                newCard.transform.parent = GameObject.Find("Cards").transform;
 
-            float x = (i / 4) * 1.4f - 2.1f;
-            float y = (i % 4) * 1.4f - 3.0f;
-            newCard.transform.position = new Vector3(x, y, 0);
+                float x = (i / 4) * 1.4f - 2.1f;
+                float y = (i % 4) * 1.4f - 3.0f;
+                newCard.transform.position = new Vector3(x, y, 0);
 
-            string name = "k_" + ks[i];
-            newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Images/"+ name);
+                string name = "h_" + ks[i];
+                newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Images/" + name);
+            }
+
         }
+
+        if (Stageindex == 1)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                GameObject newCard = Instantiate(card);
+                newCard.transform.parent = GameObject.Find("Cards").transform;
+
+                float x = (i / 4) * 1.4f - 2.1f;
+                float y = (i % 4) * 1.4f - 3.0f;
+                newCard.transform.position = new Vector3(x, y, 0);
+
+                string name = "k_" + ks[i];
+                newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Images/" + name);
+            }
+
+        }
+
+        if (Stageindex == 2)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                GameObject newCard = Instantiate(card);
+                newCard.transform.parent = GameObject.Find("Cards").transform;
+
+                float x = (i / 4) * 1.4f - 2.1f;
+                float y = (i % 4) * 1.4f - 3.0f;
+                newCard.transform.position = new Vector3(x, y, 0);
+
+                string name = "m_" + ks[i];
+                newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Images/" + name);
+            }
+
+        }
+
+
     }
 
     // Update is called once per frame
@@ -68,4 +108,6 @@ public class GameManager : MonoBehaviour
         firstCard = null;
         secondCard = null;
     }
+
+
 }
